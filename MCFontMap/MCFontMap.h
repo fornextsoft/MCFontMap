63d9c06698be818e8f0ba18e684fac3b3f2657bf
fornextsoft <Goddess@main.starfleet.loc>
2013-09-20 16:27:34 -0700

    Initial Commit

---
+++ MCFontMap/MCFontMap.h
@@ -0,0 +1,13 @@
+//
+//  MCFontMap.h
+//  MCFontMap
+//
+//  Created by April Gendill on 9/20/13.
+//  Copyright (c) 2013 April Gendill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCFontMap : NSObject
+
+@end
---
+++ MCFontMap/MCFontMap.m
@@ -0,0 +1,13 @@
+//
+//  MCFontMap.m
+//  MCFontMap
+//
+//  Created by April Gendill on 9/20/13.
+//  Copyright (c) 2013 April Gendill. All rights reserved.
+//
+
+#import "MCFontMap.h"
+
+@implementation MCFontMap
+
+@end
